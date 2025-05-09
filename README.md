ea144d98d7aa8432ef2858b9fd3101d9469b7a29
Gabriel Felipe Kuhn Schiestl <gfkschiestl@minha.fag.edu.br>
2025-05-09 11:42:16 -0300

    feat: readme

---
+++ README.md
@@ -0,0 +1,5 @@
+# Destinado para um kmeans sobre nutrientes do solo, que divide o dataset em 3 clusters:
+
+- solo com fertilidade média e pH bom
+- solo alcalino e pobre
+- solo com alto potencial produtivo
--- kmeans-agro/main.py
+++ kmeans-agro/main.py
@@ -15,3 +15,28 @@ display(true_data.head())
 
 std_data = StandardScaler().fit_transform(true_data)
 display(std_data)
+
+# %%
+import matplotlib.pyplot as plt
+from sklearn.cluster import KMeans
+
+inertia = []
+for k in range(1, 6):
+    km = KMeans(n_clusters=k, random_state=42)
+    km.fit(std_data)
+    inertia.append(km.inertia_)
+
+# Plot do Elbow
+plt.plot(range(1, 6), inertia, marker='o')
+plt.title('Elbow Method')
+plt.xlabel('Número de Clusters')
+plt.ylabel('Inércia')
+plt.grid(True)
+plt.show()
+
+# %%
+km = KMeans(n_clusters=3, random_state=42)
+
+df['Cluster'] = km.fit_predict(std_data)
+
+display(df.groupby('Cluster').mean())
